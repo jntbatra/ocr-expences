605d6fda38f3db7e809233ff5bb1c01170cd4de6
jayant batra <jayantbtra@gmail.com>
2025-11-20 23:05:21 +0530

    frontend fix

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://13.127.88.8:3000';
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://13.127.88.8:3000";
